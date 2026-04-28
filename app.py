741675d30de1578a0102ebf4f025176717b20960
johnclamy <johnclamy@gmail.com>
2026-04-28 13:36:00 +0100

    define the Task model in app.py

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+import os
 
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    completed = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
+
+
+with app.app_context():
+    if not os.path.exists('tasks.db'):
+        db.create_all()
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # tasks = Task.query.all()
+    return render_template('index.html', tasks={})
 
 
 if __name__ == '__main__':
