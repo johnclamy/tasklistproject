13fd5bcbc097c48a0ccef2752a5135f2f5372354
johnclamy <johnclamy@gmail.com>
2026-04-29 21:54:30 +0100

    change the Task model fields

--- app.py
+++ app.py
@@ -12,11 +12,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+dummyTasks = [
+    {
+        'id': 1,
+        'name': 'John Doe',
+        'title': 'Complete the project',
+        'status': False,
+        'role': 'admin',
+        'email': 'john.doe@example.com',
+        'date_created': datetime.now(timezone.utc)
+    }
+]
+
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
     title = db.Column(db.String(200), nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    completed = db.Column(db.Boolean, default=False)
+    status = db.Column(db.Boolean, default=False)
+    role = db.Column(db.String(100), default='admin')
+    email = db.Column(db.String(100), nullable=False)    
     date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
     def __repr__(self):
@@ -31,20 +46,30 @@ with app.app_context():
 @app.get('/')
 def home():
     # tasks = Task.query.all()
-    return render_template('index.html', tasks={})
+    return render_template('index.html', tasks=dummyTasks)
 
 
 @app.post('/add')
 def add():
     title = request.form.get('title', '').strip()
-    # description = request.form.get('description')
+    name = request.form.get('name', '').strip()
+    email = request.form.get('email', '').strip()
 
     if not title:
         flash("Please enter title for the task.")
         return redirect(url_for('home'))
-    
+
+    if not name:
+        flash("Please enter name for the task.")
+        return redirect(url_for('home'))
+
+    if not email:
+        flash("Please enter email for the task.")
+        return redirect(url_for('home'))
     new_task = Task()
     new_task.title = title
+    new_task.name = name
+    new_task.email = email
     # new_task.description = description
 
     db.session.add(new_task)
